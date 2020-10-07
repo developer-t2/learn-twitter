531e2bed03ac5cc61ebf639193b9d592c1347173
developer-t2 <oo_ri@naver.com>
2020-10-07 09:30:54 +0900

    Updates

---
+++ precache-manifest.9d70289f824c06ba05ca60ea8fdae001.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89bd96ae6fe801972735552d50a9a630",
+    "url": "/learn-twitter/index.html"
+  },
+  {
+    "revision": "df5a6dab22161d1d5883",
+    "url": "/learn-twitter/static/js/2.698b35eb.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/learn-twitter/static/js/2.698b35eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0eac9e1998059c5e771",
+    "url": "/learn-twitter/static/js/main.d608ea84.chunk.js"
+  },
+  {
+    "revision": "61125f73c2883e4892c2",
+    "url": "/learn-twitter/static/js/runtime-main.211407e6.js"
+  }
+]);
