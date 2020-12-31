0a8d987c2610b970c4f8eb2b5587f92e32a98ea8
t2 <oo_ri@naver.com>
2020-12-31 18:28:45 +0900

    Updates

---
+++ precache-manifest.6fa8fa99513dd005dfc00095ebb25e2e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e931afc5a29fa996877e7c80cca05217",
+    "url": "/twitter-clone/index.html"
+  },
+  {
+    "revision": "9e9b9dffad128f88c024",
+    "url": "/twitter-clone/static/js/2.0530ff30.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/twitter-clone/static/js/2.0530ff30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4f4b1148b6dd2741cec",
+    "url": "/twitter-clone/static/js/main.6282ec78.chunk.js"
+  },
+  {
+    "revision": "38c039c4bc0f3a478a1b",
+    "url": "/twitter-clone/static/js/runtime-main.78e26e91.js"
+  }
+]);
